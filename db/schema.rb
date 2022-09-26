6e3ebc54c0b76c11d856e63d28a8ca9960a0f42d
AlexMoree <alex.d.moree@gmail.com>
2022-09-25 19:15:53 -0500

    added unit/integration testing and SimpleCov

--- db/schema.rb
+++ db/schema.rb
@@ -53,9 +53,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_055546) do
   create_table "qr_codes", force: :cascade do |t|
     t.integer "event_id"
     t.string "attendence_link"
-    t.string "signup_link"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
---
+++ spec/feature/integration_spec.rb
@@ -0,0 +1,21 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a new personal information account', type: :feature do
+  scenario 'valid inputs' do
+    visit new_personal_information_path
+    fill_in 'Student', with: '1111111111'
+    fill_in 'First name', with: 'Jared'
+    fill_in 'Last name', with: 'Smith'
+    fill_in 'Classification', with: 'Construction Engineering'
+    fill_in 'Major', with: 'Freshmam'
+    click_on 'Create Personal Information'
+    visit personal_information_path
+    expect(page).to have_content('1111111111')
+    expect(page).to have_content('Jared')
+    expect(page).to have_content('Smith')
+    expect(page).to have_content('Construction Engineering')
+    expect(page).to have_content('Freshmam')
+
+  end
+end
