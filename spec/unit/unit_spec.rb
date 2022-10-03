f59bfe49684f5d6124eabe47ccee12cb403c2cbb
AlexMoree <alex.d.moree@gmail.com>
2022-10-02 22:49:29 -0500

    Unit testing working for Attendance

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,17 +1,28 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
 
-RSpec.describe(personal_information, type: :model) do
+RSpec.describe(Attendence, type: :model) do
   subject do
-    described_class.new(Uin: '111111111')
+    described_class.new(uin: '1010001101', day_of_event: '10/2/2022')
   end
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
+  it 'is not valid without both' do
+    subject.uin = nil
+    subject.day_of_event = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a uin' do
+    subject.uin = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.day_of_event = nil
     expect(subject).not_to be_valid
   end
 end
