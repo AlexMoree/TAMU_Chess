57d3f2f3f975641ce804613ef235a66972c05627
sanghlee98 <sanghlee98@gmail.com>
2022-11-02 18:57:31 -0500

    personal_information uin uniqueness per semester

--- app/models/attendence.rb
+++ app/models/attendence.rb
@@ -2,5 +2,8 @@ class Attendence < ApplicationRecord
     has_many :personal_informations
 
     validates :uin, length: { minimum: 9, maximum: 9 },numericality: { only_integer: true }
+
+    
+
     validates :uin, :day_of_event, presence: true
 end
