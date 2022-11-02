36679f61691ef06baad1b4c6f2cb53f4a1f5c35b
sanghlee98 <sanghlee98@gmail.com>
2022-11-01 22:00:38 -0500

    attendance and qr_code validation

--- app/models/qr_code.rb
+++ app/models/qr_code.rb
@@ -1,3 +1,5 @@
 class QrCode < ApplicationRecord
+
+    validates :qr_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "incorrect url format" }
     validates :title, :qr_url, presence: true
 end
