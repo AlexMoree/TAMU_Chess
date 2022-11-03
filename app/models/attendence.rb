de0e9de3754234d2ac26b0f10c66df65cc0cefde
sanghlee98 <sanghlee98@gmail.com>
2022-11-02 19:06:30 -0500

    attendance uin uniqueness on same date, date uniqueness on same uin

--- app/models/attendence.rb
+++ app/models/attendence.rb
@@ -3,6 +3,7 @@
 class Attendence < ApplicationRecord
   has_many :personal_informations
 
-  validates :uin, length: { minimum: 9, maximum: 9 }, numericality: { only_integer: true }
+  validates :uin, length: { minimum: 9, maximum: 9 }, numericality: { only_integer: true }, uniqueness: { scope: :day_of_event }
+  validates :day_of_event, uniqueness: { scope: :uin }
   validates :uin, :day_of_event, presence: true
 end
