b93c808119153d3740442825515af466b9035505
AlexMoree <alex.d.moree@gmail.com>
2022-11-09 14:33:36 -0600

    made uin correctly

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -285,6 +285,11 @@ class StatisticsController < ApplicationController
       actual_name = "#{personal_information.first_name} #{personal_information.last_name}"
       puts 'actual_Name                 :'
       puts actual_name
+
+      uin = personal_information.uin
+      puts 'uin'
+      puts uin
+
       current_username_one = personal_information.lichess_org_username
       current_username_two = personal_information.chess_com_username
       puts 'lichess username:'
@@ -303,7 +308,7 @@ class StatisticsController < ApplicationController
 
       statistic = Statistic.new
       statistic.actual_name = actual_name
-      statistic.uin = personal_information.uin
+      statistic.uin = uin
       statistic.lichess_username = current_username_one
       statistic.lichess_blitz = current_stats_one[0]
       statistic.lichess_rapid = current_stats_one[1]
